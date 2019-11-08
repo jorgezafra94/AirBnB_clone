fbba4f5e82d3329c251733e4707dace110a8d40c
Carlos Molano <926@holbertonschool.com>
2019-11-08 16:42:57 -0500

    [Update] console.py: adding create method

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import cmd
 import sys
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -18,6 +19,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Does not perform any action """
         return (False)
+
+    def do_create(self, args):
+        """ Creates an object using the BaseModel """
+        if args is None or len(args) == 0:
+            print("** class name missing **")
+        else:
+            try:
+                new = eval(str(args) + "()")
+                new.save()
+                print(new.id)
+            except:
+                print("** class doesn't exist **")
 
 if __name__ == '__main__':
     """ Main """

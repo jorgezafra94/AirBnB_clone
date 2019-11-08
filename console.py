19672e74e11205a24c04e81a3880d9d58416ce9f
Carlos Molano <926@holbertonschool.com>
2019-11-08 17:57:58 -0500

    [Update] console.py: adding show method

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 import cmd
 import sys
 from models.base_model import BaseModel
+import models
 
 
 class HBNBCommand(cmd.Cmd):
@@ -35,6 +36,32 @@ class HBNBCommand(cmd.Cmd):
                 print(new.id)
             except:
                 print("** class doesn't exist **")
+
+    def do_show(self, args):
+        """
+        Prints the string representation of an instance
+        based on the class name and id
+        eval the class if it exist or not
+        var = eval("BaseModel") is the same as var = Basemodel()
+        """
+        if args is None or len(args) == 0:
+            print("** class name missing **")
+        else:
+            line = args.split(' ')
+
+            if len(line) < 2:
+                print("""** instance id missing **""")
+            else:
+
+                try:
+                    objects = models.storage.all()
+                    key = str(line[0]) + "." + str(line[1])
+                    if key in objects:
+                        print(objects[key])
+                    else:
+                        raise KeyError()
+                except:
+                    print("** class doesn't exist **")
 
 if __name__ == '__main__':
     """ Main """

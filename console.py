d065c8a280288dee20e5070acc35b096415542ce
jorgezafra94 <947@holbertonschool.com>
2019-11-09 15:16:08 +0000

    [Update] documentation

--- console.py
+++ console.py
@@ -26,7 +26,7 @@ class HBNBCommand(cmd.Cmd):
         """
         Creates an object using the BaseModel
         eval the class if it exist or not
-        var = eval("BaseModel") is the same as var = Basemodel()
+        var = eval("BaseModel()") is the same as var = Basemodel()
         """
         if args is None or len(args) == 0:
             print("** class name missing **")
@@ -44,6 +44,32 @@ class HBNBCommand(cmd.Cmd):
         based on the class name and id
         eval the class if it exist or not
         var = eval("BaseModel") is the same as var = Basemodel()
+        use of split in order to get tokens or args
+        """
+        if args is None or len(args) == 0:
+            print("** class name missing **")
+        else:
+            line = args.split(' ')
+
+            if len(line) < 2:
+                print("""** instance id missing **""")
+            else:
+                try:
+                    objects = models.storage.all()
+                    key = str(line[0]) + "." + str(line[1])
+                    if key in objects:
+                        print(objects[key])
+                    else:
+                        print("** no instance found **")
+                except:
+                    print("** class doesn't exist **")
+
+    def do_destroy(self, args):
+        """
+        delete the object: the dictionary of dictionaries with storage.all()
+        using alias with variable objects, and remove the key specified from
+        objects where modifies at the same time self.__objects Cuz is an alias
+        and finally update json file with storage.save()
         """
         if args is None or len(args) == 0:
             print("** class name missing **")
@@ -58,55 +84,35 @@ class HBNBCommand(cmd.Cmd):
                     objects = models.storage.all()
                     key = str(line[0]) + "." + str(line[1])
                     if key in objects:
-                        print(objects[key])
-                    else:
-                        print("** no instance found **")
-                except:
-                    print("** class doesn't exist **")
-
-    def do_destroy(self, args):
-        """delete the object and update json file"""
-        if args is None or len(args) == 0:
-            print("** class name missing **")
-        else:
-            line = args.split(' ')
-
-            if len(line) < 2:
-                print("""** instance id missing **""")
-            else:
-
-                try:
-                    objects = models.storage.all()
-                    key = str(line[0]) + "." + str(line[1])
-                    if key in objects:
                         del(objects[key])
-                        new = {}
-                        for elem in objects:
-                            new[elem] = str(objects[elem])
-                        with open("file.json", 'w') as fd:
-                            fd.write(json.dumps(new))
+                        models.storage.save()
                     else:
                         print("** no instance found **")
                 except:
                     print("** class doesn't exist **")
 
     def do_all(self, args):
-        """Prints all string representation of all instances based or not
-        on the class name"""
+        """
+        Prints all string representation of all instances based or not
+        on the class name
+        we have to use str because we have to call the method __str__
+        in order to print in the format that we need
+        """
         objects = models.storage.all()
         objList = []
         if args is "":
             for key in objects:
                 objList.append(str(objects[key]))
             print(objList)
-        try:
-            line = args.split(" ")
-            eval(line[0])
-            for key in objects:
-                objList.append(str(objects[key]))
-            print(objList)
-        except:
-            print("** class doesn't exist **")
+        else:
+            try:
+                line = args.split(" ")
+                eval(line[0])
+                for key in objects:
+                    objList.append(str(objects[key]))
+                print(objList)
+            except:
+                print("** class doesn't exist **")
 
 if __name__ == '__main__':
     """ Main """

b8119aa22d14ad62a13b12acf44ad8fe1a8356ea
Carlos Molano <926@holbertonschool.com>
2019-11-12 15:48:05 -0500

    [Fix] console

--- console.py
+++ console.py
@@ -44,9 +44,6 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """Create a new instance - Usage: create <Classname>\n"""
-        # Creates an object using the BaseModel
-        # eval the class if it exist or not
-        # var = eval("BaseModel()") is the same as var = Basemodel()
         if args is None or len(args) == 0:
             print("** class name missing **")
         else:
@@ -59,11 +56,6 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, args):
         """Show the representation of an instance - Usage show <Classname>\n"""
-        # Prints the string representation of an instance
-        # based on the class name and id
-        # eval the class if it exist or not
-        # var = eval("BaseModel") is the same as var = Basemodel()
-        # use of split in order to get tokens or args
         if args is None or len(args) == 0:
             print("** class name missing **")
         else:
@@ -85,10 +77,6 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, args):
         """Delete the instance of a given class -
         Usage: destroy <classname> <id>\n"""
-        # delete the object: the dictionary of dictionaries with storage.all()
-        # using alias with variable objects, and remove the key specified from
-        # objects where modifies at the same time self.__objects Cuz is
-        # an alias and finally update json file with storage.save()
         if args is None or len(args) == 0:
             print("** class name missing **")
         else:
@@ -111,10 +99,6 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, args):
         """Print all instances of a class - Usage: all <classname>\n"""
-        # Prints all string representation of all instances based or not
-        # on the class name
-        # we have to use str because we have to call the method __str__
-        # in order to print in the format that we need
         objects = models.storage.all()
         objList = []
         if args is "":
@@ -136,8 +120,6 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, args):
         """Update or set att in an instance -
         Usage: update <classname> <id> <att_name> <att_value>\n"""
-        # Updates an instance based on the class name and id by adding
-        # or updating attribute
         line = shlex.split(args)
         if len(line) == 0:
             print("** class name missing **")
@@ -181,8 +163,6 @@ class HBNBCommand(cmd.Cmd):
         first = args.split('.')
         if len(first) > 1:
             class_name = first[0]
-            # now we have to separate the first[1] using ( delimiter
-            # that is why we add a &
             methods = first[1]
             first[1] = first[1].replace('(', '&(')
             second = first[1].split('&')
@@ -193,10 +173,8 @@ class HBNBCommand(cmd.Cmd):
             elif methods == "count()":
                 self.do_count(comando)
             else:
-                # overwrite the method without ()
                 methods = second[0]
                 elems = second[1]
-                # remove (), and separate each elem
                 elems = elems.replace('(', '')
                 elems = elems.replace(')', '')
                 elems = elems.replace('{', '"{')
@@ -208,8 +186,6 @@ class HBNBCommand(cmd.Cmd):
                 id = third[0]
                 comando = comando + ' ' + id
                 comando = comando.replace('\"', '')
-                # read if len(third) > 1 error for show because it only
-                # allows one element same with destroy
                 if methods == "show" and len(third) == 1:
                     self.do_show(comando)
                 elif methods == "destroy" and len(third) == 1:

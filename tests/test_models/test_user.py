fb6bde5a412cebad6ef6b6ace2445b7692f94161
Carlos Molano <926@holbertonschool.com>
2019-11-12 17:41:43 -0500

    [fix] test_user: re-writing file

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -20,11 +20,6 @@ class UserTest(unittest.TestCase):
         cls.my_model.password = "12345"
         cls.my_model.first_name = "Carlos"
         cls.my_model.last_name = "Molano"
-        cls.my_model2 = User()
-        cls.my_model2.email = None
-        cls.my_model2.password = None
-        cls.my_model2.first_name = None
-        cls.my_model2.last_name = None
 
     @classmethod
     def teardown(cls):
@@ -41,29 +36,35 @@ class UserTest(unittest.TestCase):
         self.assertIsNotNone(User.__doc__)
 
     def test_subclass_instance_User(self):
-        """Test if my_model 1 and 2 are subclasses of BaseModel,
-        also if they are instances of Amenity
-        if are instances checks if the attributes were well
-        assigned"""
+        """Test if my_model 1 and 2 are subclasses of BaseModel"""
         self.assertTrue(issubclass(self.my_model.__class__, BaseModel))
-        self.assertTrue(issubclass(self.my_model2.__class__, BaseModel))
-        self.assertTrue(isinstance(self.my_model, BaseModel))
-        self.assertTrue(isinstance(self.my_model2, BaseModel))
         self.assertTrue(isinstance(self.my_model, User))
-        self.assertTrue(isinstance(self.my_model2, User))
-        self.assertEqual(self.my_model.email, "123@hotmail.com")
-        self.assertEqual(self.my_model.password, "12345")
-        self.assertEqual(self.my_model.first_name, "Carlos")
-        self.assertEqual(self.my_model.last_name, "Molano")
-        self.assertEqual(self.my_model2.email, None)
-        self.assertEqual(self.my_model2.password, None)
-        self.assertEqual(self.my_model2.first_name, None)
-        self.assertEqual(self.my_model2.last_name, None)
 
-    def test_diff_instances_User(self):
-        """ Test if two instences were created at different time
-        and have different id's"""
-        self.assertNotEqual(self.my_model.id, self.my_model2.id)
+    def test_attribute_email(self):
+        """ Tests email """
+        self.assertTrue(hasattr(self.my_model, 'email'))
+        self.assertEqual(self.my_model.email, "123@hotmail.com")
+
+    def test_attribute_password(self):
+        """ Test password """
+        self.assertTrue(hasattr(self.my_model, 'password'))
+        self.assertEqual(self.my_model.password, "12345")
+
+    def test_attribute_first_name(self):
+        """ Check first name """
+        self.assertTrue(hasattr(self.my_model, 'first_name'))
+        self.assertEqual(self.my_model.first_name, "Carlos")
+
+    def test_attribute_last_name(self):
+        """Check last name"""
+        self.assertTrue(hasattr(self.my_model, 'last_name'))
+        self.assertEqual(self.my_model.last_name, "Molano")
+
+    def test_hasattr(self):
+        """ attributes inheritated of BaseModel"""
+        self.assertTrue(hasattr(self.my_model, 'id'))
+        self.assertTrue(hasattr(self.my_model, 'created_at'))
+        self.assertTrue(hasattr(self.my_model, 'updated_at'))
 
     def test_str(self):
         """Test if __str__ method show the right output"""
@@ -76,7 +77,7 @@ class UserTest(unittest.TestCase):
         self.my_model.save()
         self.assertNotEqual(self.my_model.created_at, self.my_model.updated_at)
 
-    def test_to_dict_Amenity(self):
+    def test_to_dict_User(self):
         """If the convertion to dictionary works:
         __class__: has to be created
         created_at and updated at have to change the format"""
@@ -105,33 +106,6 @@ class UserTest(unittest.TestCase):
         self.assertEqual(my_new_model.created_at, self.my_model.created_at)
         self.assertEqual(my_new_model.updated_at, self.my_model.updated_at)
         self.assertNotEqual(my_new_model, self.my_model)
-
-    def test_hasattr1(self):
-        """ attributes of User Class"""
-        new = User()
-        self.assertTrue(hasattr(new, 'email'))
-
-    def test_hasattr2(self):
-        """ attributes of User Class"""
-        new = User()
-        self.assertTrue(hasattr(new, 'password'))
-
-    def test_hasattr3(self):
-        """ attributes of User Class"""
-        new = User()
-        self.assertTrue(hasattr(new, 'first_name'))
-
-    def test_hasattr4(self):
-        """ attributes of User Class"""
-        new = User()
-        self.assertTrue(hasattr(new, 'last_name'))
-
-    def test_hasattr5(self):
-        """ attributes inheritated of BaseModel"""
-        new = User()
-        self.assertTrue(hasattr(new, 'id'))
-        self.assertTrue(hasattr(new, 'created_at'))
-        self.assertTrue(hasattr(new, 'updated_at'))
 
 if __name__ == '__main__':
     unittest.main()

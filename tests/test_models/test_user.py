5efd1a81737dfd3cf757faeba737e52f97be1ae7
Carlos Molano <926@holbertonschool.com>
2019-11-12 18:40:21 -0500

    [fix] test_user: re-writing file

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,15 +4,13 @@
 from models.user import User
 import datetime
 import unittest
+from models.base_model import BaseModel
 
 
-class UserTest(unittest.TestCase):
+class TestUser(unittest.TestCase):
     """ Test User Class """
-    my_model = User()
-    my_model.email = "123@hotmail.com"
-    my_model.password = "12345"
-    my_model.first_name = "Carlos"
-    my_model.last_name = "Molano"
+    model = User()
+    model.name = "Betty"
 
     def test_checking_for_docstring_User(self):
         """Test if all docstring were written"""
@@ -20,42 +18,39 @@ class UserTest(unittest.TestCase):
 
     def test_subclass_instance_User(self):
         """Test if my_model 1 and 2 are subclasses of BaseModel"""
-        self.assertTrue(isinstance(self.my_model, User))
+        self.assertTrue(isinstance(self.model, User))
 
     def test_attribute_email(self):
         """ Tests email """
-        self.assertTrue(hasattr(self.my_model, 'email'))
-        self.assertEqual(self.my_model.email, "123@hotmail.com")
+        self.assertTrue(hasattr(self.model, 'email'))
 
     def test_attribute_password(self):
         """ Test password """
-        self.assertTrue(hasattr(self.my_model, 'password'))
-        self.assertEqual(self.my_model.password, "12345")
+        self.assertTrue(hasattr(self.model, 'password'))
 
     def test_attribute_first_name(self):
         """ Check first name """
-        self.assertTrue(hasattr(self.my_model, 'first_name'))
-        self.assertEqual(self.my_model.first_name, "Carlos")
+        self.assertTrue(hasattr(self.model, 'first_name'))
 
     def test_attribute_last_name(self):
         """Check last name"""
-        self.assertTrue(hasattr(self.my_model, 'last_name'))
-        self.assertEqual(self.my_model.last_name, "Molano")
+        self.assertTrue(hasattr(self.model, 'last_name'))
 
     def test_hasattr(self):
         """ attributes inheritated of BaseModel"""
-        self.assertTrue(hasattr(self.my_model, 'id'))
-        self.assertTrue(hasattr(self.my_model, 'created_at'))
-        self.assertTrue(hasattr(self.my_model, 'updated_at'))
+        self.assertTrue(hasattr(self.model, 'name'))
+        self.assertTrue(hasattr(self.model, 'id'))
+        self.assertTrue(hasattr(self.model, 'created_at'))
+        self.assertTrue(hasattr(self.model, 'updated_at'))
 
     def test_attributes_types(self):
         """Tests types """
-        self.assertEqual(type(self.my_model.email), str)
-        self.assertEqual(type(self.my_model.last_name), str)
-        self.assertEqual(type(self.my_model.first_name), str)
-        self.assertEqual(type(self.my_model.password), str)
-        self.assertIsInstance(self.my_model.created_at, datetime.datetime)
-        self.assertIsInstance(self.my_model.updated_at, datetime.datetime)
+        self.assertEqual(type(self.model.email), str)
+        self.assertEqual(type(self.model.last_name), str)
+        self.assertEqual(type(self.model.first_name), str)
+        self.assertEqual(type(self.model.password), str)
+        self.assertIsInstance(self.model.created_at, datetime.datetime)
+        self.assertIsInstance(self.model.updated_at, datetime.datetime)
 
 if __name__ == '__main__':
     unittest.main()

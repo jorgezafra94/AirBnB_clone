2b4837e0b471f262b7bfb6a8076b69ffae648df3
Carlos Molano <926@holbertonschool.com>
2019-11-12 19:03:45 -0500

    [Fix] unittest

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,100 +1,46 @@
 #!/usr/bin/python3
-""" Test of City"""
+"""Test of City Class """
 
-
-import unittest
-import os
-from models.base_model import BaseModel
 from models.city import City
-from datetime import datetime
+import datetime
+import unittest
+from models.base_model import BaseModel
 
 
-class CityTest(unittest.TestCase):
-    """Tests for City Class"""
+class TestCity(unittest.TestCase):
+    """ Test City Class """
+    model = City()
+    model.name = "Betty"
 
-    @classmethod
-    def setUpClass(cls):
-        """ Setup an instance for test"""
-        cls.my_model = City()
-        cls.my_model.name = "Bogota"
-        cls.my_model.state_id = "001"
-        cls.my_model2 = City()
-        cls.my_model2.name = "Medellin"
-        cls.my_model2.state_id = "002"
-
-    @classmethod
-    def teardown(cls):
-        """ Delete the instance at the end of tests"""
-        del cls.my_model
-        del cls.my_model2
-        try:
-            os.remove("file.json")
-        except:
-            pass
-
-    def test_checking_for_docstring_City(self):
+    def test_checking_for_docstring_User(self):
         """Test if all docstring were written"""
         self.assertIsNotNone(City.__doc__)
 
-    def test_subclass_instance_City(self):
-        """Test if my_model 1 and 2 are subclasses of BaseModel,
-        also if they are instances of City
-        if are instances checks if the attributes were well
-        assigned"""
-        self.assertTrue(issubclass(self.my_model.__class__, BaseModel))
-        self.assertTrue(issubclass(self.my_model2.__class__, BaseModel))
-        self.assertTrue(isinstance(self.my_model, BaseModel))
-        self.assertTrue(isinstance(self.my_model2, BaseModel))
-        self.assertTrue(isinstance(self.my_model, City))
-        self.assertTrue(isinstance(self.my_model2, City))
-        self.assertEqual(self.my_model.name, "Bogota")
-        self.assertEqual(self.my_model.state_id, "001")
-        self.assertEqual(self.my_model2.name, "Medellin")
-        self.assertEqual(self.my_model2.state_id, "002")
+    def test_subclass_instance_User(self):
+        """Test if my_model 1 and 2 are subclasses of BaseModel"""
+        self.assertTrue(isinstance(self.model, City))
 
-    def test_diff_instances_City(self):
-        """ Test if two instences were created at different time
-        and have different id's"""
-        self.assertNotEqual(self.my_model.id, self.my_model2.id)
+    def test_attribute_state_id(self):
+        """ Tests email """
+        self.assertTrue(hasattr(self.model, 'state_id'))
 
-    def test_str(self):
-        """Test if __str__ method show the right output"""
-        string = "[City] ({}) {}".format(self.my_model.id,
-                                         self.my_model.__dict__)
-        self.assertEqual(string, str(self.my_model))
+    def test_attribute_name(self):
+        """ Test password """
+        self.assertTrue(hasattr(self.model, 'name'))
 
-    def test_save_City(self):
-        """Test if updated at changes"""
-        self.my_model.save()
-        self.assertNotEqual(self.my_model.created_at, self.my_model.updated_at)
+    def test_hasattr(self):
+        """ attributes inheritated of BaseModel"""
+        self.assertTrue(hasattr(self.model, 'name'))
+        self.assertTrue(hasattr(self.model, 'id'))
+        self.assertTrue(hasattr(self.model, 'created_at'))
+        self.assertTrue(hasattr(self.model, 'updated_at'))
 
-    def test_to_dict_City(self):
-        """If the convertion to dictionary works:
-        __class__: has to be created
-        created_at and updated at have to change the format"""
-        t_format = "%Y-%m-%dT%H:%M:%S.%f"
-        model_dict = self.my_model.to_dict()
-        self.assertEqual(self.my_model.__class__.__name__, 'City')
-        self.assertEqual(type(model_dict['created_at']), str)
-        self.assertEqual(type(model_dict['updated_at']), str)
-        self.assertEqual(model_dict["created_at"],
-                         self.my_model.created_at.strftime(t_format))
-        self.assertEqual(model_dict["updated_at"],
-                         self.my_model.updated_at.strftime(t_format))
-
-    def test_from_dict_to_City(self):
-        """Test if we can create an instance from a dictionary"""
-        my_model_json = self.my_model.to_dict()
-        my_new_model = City(**my_model_json)
-        self.assertTrue(isinstance(my_new_model, City))
-        self.assertTrue(isinstance(my_new_model, BaseModel))
-        self.assertTrue(issubclass(my_new_model.__class__, BaseModel))
-        self.assertEqual(my_new_model.name, "Bogota")
-        self.assertEqual(my_new_model.state_id, "001")
-        self.assertEqual(my_new_model.id, self.my_model.id)
-        self.assertEqual(my_new_model.created_at, self.my_model.created_at)
-        self.assertEqual(my_new_model.updated_at, self.my_model.updated_at)
-        self.assertNotEqual(my_new_model, self.my_model)
+    def test_attributes_types(self):
+        """Tests types """
+        self.assertEqual(type(self.model.name), str)
+        self.assertEqual(type(self.model.state_id), str)
+        self.assertIsInstance(self.model.created_at, datetime.datetime)
+        self.assertIsInstance(self.model.updated_at, datetime.datetime)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,97 +1,41 @@
 #!/usr/bin/python3
-""" Test of State Class"""
+"""Test of State Class """
 
-
-import unittest
-import os
-from models.base_model import BaseModel
 from models.state import State
-from datetime import datetime
+import datetime
+import unittest
+from models.base_model import BaseModel
 
 
-class StateTest(unittest.TestCase):
-    """Tests for State Class"""
+class TestUser(unittest.TestCase):
+    """ Test User Class """
+    model = State()
+    model.name = "Betty"
 
-    @classmethod
-    def setUpClass(cls):
-        """ Setup an instance for test"""
-        cls.my_model = State()
-        cls.my_model.name = "Bogota D.C."
-        cls.my_model2 = State()
-        cls.my_model2.name = "Antioquia"
-
-    @classmethod
-    def teardown(cls):
-        """ Delete the instance at the end of tests"""
-        del cls.my_model
-        del cls.my_model2
-        try:
-            os.remove("file.json")
-        except:
-            pass
-
-    def test_checking_for_docstring_State(self):
+    def test_checking_for_docstring_User(self):
         """Test if all docstring were written"""
         self.assertIsNotNone(State.__doc__)
 
-    def test_subclass_instance_State(self):
-        """Test if my_model 1 and 2 are subclasses of BaseModel,
-        also if they are instances of State
-        if are instances checks if the attributes were well
-        assigned"""
-        self.assertTrue(issubclass(self.my_model.__class__, BaseModel))
-        self.assertTrue(issubclass(self.my_model2.__class__, BaseModel))
-        self.assertTrue(isinstance(self.my_model, BaseModel))
-        self.assertTrue(isinstance(self.my_model2, BaseModel))
-        self.assertTrue(isinstance(self.my_model, State))
-        self.assertTrue(isinstance(self.my_model2, State))
-        self.assertEqual(self.my_model.name, "Bogota D.C.")
-        self.assertEqual(self.my_model2.name, "Antioquia")
+    def test_subclass_instance_User(self):
+        """Test if my_model 1 and 2 are subclasses of BaseModel"""
+        self.assertTrue(isinstance(self.model, State))
 
-    def test_diff_instances_State(self):
-        """ Test if two instences were created at different time
-        and have different id's"""
-        self.assertNotEqual(self.my_model.created_at,
-                            self.my_model2.created_at)
-        self.assertNotEqual(self.my_model.id, self.my_model2.id)
+    def test_attribute_name(self):
+        """ Tests name """
+        self.assertTrue(hasattr(self.model, 'name'))
 
-    def test_str(self):
-        """Test if __str__ method show the right output"""
-        string = "[State] ({}) {}".format(self.my_model.id,
-                                          self.my_model.__dict__)
-        self.assertEqual(string, str(self.my_model))
+    def test_hasattr(self):
+        """ attributes inheritated of BaseModel"""
+        self.assertTrue(hasattr(self.model, 'name'))
+        self.assertTrue(hasattr(self.model, 'id'))
+        self.assertTrue(hasattr(self.model, 'created_at'))
+        self.assertTrue(hasattr(self.model, 'updated_at'))
 
-    def test_save_State(self):
-        """Test if updated at changes"""
-        self.my_model.save()
-        self.assertNotEqual(self.my_model.created_at, self.my_model.updated_at)
-
-    def test_to_dict_State(self):
-        """If the convertion to dictionary works:
-        __class__: has to be created
-        created_at and updated at have to change the format"""
-        t_format = "%Y-%m-%dT%H:%M:%S.%f"
-        model_dict = self.my_model.to_dict()
-        self.assertEqual(self.my_model.__class__.__name__, 'State')
-        self.assertEqual(type(model_dict['created_at']), str)
-        self.assertEqual(type(model_dict['updated_at']), str)
-        self.assertEqual(model_dict["created_at"],
-                         self.my_model.created_at.strftime(t_format))
-        self.assertEqual(model_dict["updated_at"],
-                         self.my_model.updated_at.strftime(t_format))
-
-    def test_from_dict_to_State(self):
-        """Test if we can create an instance from a dictionary"""
-        my_model_json = self.my_model.to_dict()
-        my_new_model = State(**my_model_json)
-        self.assertTrue(isinstance(my_new_model, State))
-        self.assertTrue(isinstance(my_new_model, BaseModel))
-        self.assertTrue(issubclass(my_new_model.__class__, BaseModel))
-        self.assertEqual(my_new_model.name, "Bogota D.C.")
-        self.assertEqual(my_new_model.id, self.my_model.id)
-        self.assertEqual(my_new_model.created_at, self.my_model.created_at)
-        self.assertEqual(my_new_model.updated_at, self.my_model.updated_at)
-        self.assertNotEqual(my_new_model, self.my_model)
+    def test_attributes_types(self):
+        """Tests types """
+        self.assertEqual(type(self.model.name), str)
+        self.assertIsInstance(self.model.created_at, datetime.datetime)
+        self.assertIsInstance(self.model.updated_at, datetime.datetime)
 
 if __name__ == '__main__':
     unittest.main()

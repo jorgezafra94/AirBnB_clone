cb31670991004a0e8e642fb6516ad1f05350d991
jorgezafra94 <947@holbertonschool.com>
2019-11-13 17:16:28 +0000

    [fix] remove some test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Test of Amenity Class """
+"""
+Test of Amenity Class
+x
+"""
 
 from models.amenity import Amenity
 import datetime
@@ -10,7 +13,6 @@ from models.base_model import BaseModel
 class TestAmenity(unittest.TestCase):
     """ Test User Class """
     model = Amenity()
-    model.name = "Betty"
 
     def test_checking_for_docstring(self):
         """Test if all docstring were written"""
@@ -18,24 +20,11 @@ class TestAmenity(unittest.TestCase):
 
     def test_subclass_instance_User(self):
         """Test if my_model 1 and 2 are subclasses of BaseModel"""
-        self.assertEqual(isinstance(self.model, Amenity), True)
+        self.assertTrue(isinstance(self.model, Amenity))
 
     def test_attribute_name(self):
         """Check name"""
         self.assertEqual(hasattr(self.model, 'name'), True)
-
-    def test_hasattr(self):
-        """ attributes inheritated of BaseModel"""
-        self.assertEqual(hasattr(self.model, 'name'), True)
-        self.assertEqual(hasattr(self.model, 'id'), True)
-        self.assertEqual(hasattr(self.model, 'created_at'), True)
-        self.assertEqual(hasattr(self.model, 'updated_at'), True)
-
-    def test_attributes_types(self):
-        """Tests types """
-        self.assertEqual(type(self.model.name), str)
-        self.assertIsInstance(self.model.created_at, datetime.datetime)
-        self.assertIsInstance(self.model.updated_at, datetime.datetime)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -10,66 +10,28 @@ from models.base_model import BaseModel
 class TestPlace(unittest.TestCase):
     """ Test Place Class """
     model = Place()
-    model.name = "Betty"
 
     def test_checking_for_docstring(self):
         """Test if all docstring were written"""
         self.assertIsNotNone(Place.__doc__)
 
-    def test_subclass_instance(self):
+    def test_instance(self):
         """Test if my_model 1 and 2 are subclasses of BaseModel"""
-        self.assertEqual(isinstance(self.model, Place), True)
+        self.assertTrue(isinstance(self.model, Place))
 
     def test_attribute_city_id(self):
-        """ Tests city_id """
-        self.assertEqual(hasattr(self.model, 'city_id'), True)
-
-    def test_attribute_user_id(self):
-        """ Test user_id """
-        self.assertEqual(hasattr(self.model, 'user_id'), True)
-
-    def test_attribute_name(self):
-        """ Check name """
-        self.assertEqual(hasattr(self.model, 'name'), True)
-
-    def test_attribute_description(self):
-        """Check description"""
-        self.assertEqual(hasattr(self.model, 'description'), True)
-
-    def test_attribute_number_rooms(self):
-        """Check number_rooms"""
-        self.assertEqual(hasattr(self.model, 'number_rooms'), True)
-
-    def test_attribute_number_bathrooms(self):
-        """Check number_bathrooms"""
-        self.assertEqual(hasattr(self.model, 'number_bathrooms'), True)
-
-    def test_attribute_max_guest(self):
-        """Check max_guest"""
-        self.assertEqual(hasattr(self.model, 'max_guest'), True)
-
-    def test_attribute_price_by_night(self):
-        """Check price_by_night"""
-        self.assertEqual(hasattr(self.model, 'price_by_night'), True)
-
-    def test_attribute_latitude(self):
-        """Check latitude"""
-        self.assertEqual(hasattr(self.model, 'latitude'), True)
-
-    def test_attribute_longitude(self):
-        """Check longitude"""
-        self.assertEqual(hasattr(self.model, 'longitude'), True)
-
-    def test_attribute_amenity_ids(self):
-        """Check amenity_ids"""
-        self.assertEqual(hasattr(self.model, 'amenity_ids'), True)
-
-    def test_hasattr(self):
-        """ attributes inheritated of BaseModel"""
-        self.assertEqual(hasattr(self.model, 'name'), True)
-        self.assertEqual(hasattr(self.model, 'id'), True)
-        self.assertEqual(hasattr(self.model, 'created_at'), True)
-        self.assertEqual(hasattr(self.model, 'updated_at'), True)
+        """ Tests attributes """
+        self.assertEqual(hasattr(self.model, "city_id"), True)
+        self.assertEqual(hasattr(self.model, "user_id"), True)
+        self.assertEqual(hasattr(self.model, "name"), True)
+        self.assertEqual(hasattr(self.model, "description"), True)
+        self.assertEqual(hasattr(self.model, "number_rooms"), True)
+        self.assertEqual(hasattr(self.model, "number_bathrooms"), True)
+        self.assertEqual(hasattr(self.model, "max_guest"), True)
+        self.assertEqual(hasattr(self.model, "price_by_night"), True)
+        self.assertEqual(hasattr(self.model, "latitude"), True)
+        self.assertEqual(hasattr(self.model, "longitude"), True)
+        self.assertEqual(hasattr(self.model, "amenity_ids"), True)
 
 if __name__ == '__main__':
     unittest.main()

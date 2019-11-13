15d2c0795a2a1c09c4d239735837eedb1de380d6
jorgezafra94 <947@holbertonschool.com>
2019-11-13 17:57:30 +0000

    [update] adding type

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -16,11 +16,15 @@ class TestUser(unittest.TestCase):
 
     def test_subclass_instance_User(self):
         """Test if my_model 1 and 2 are subclasses of BaseModel"""
-        self.assertTrue(isinstance(self.model, State))
+        self.assertIsInstance(self.model, State)
 
     def test_attribute(self):
         """ Tests attr """
         self.assertEqual(hasattr(self.model, "name"), True)
+
+    def test_types(self):
+        """ test types"""
+        self.assertEqual(type(self.model.name), str)
 
 if __name__ == '__main__':
     unittest.main()
